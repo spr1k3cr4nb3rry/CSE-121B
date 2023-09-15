864c52893a161c6427ebfd2126278433a953820e
Izzie Vazquez <izzievazquez@gmail.com>
2023-09-14 21:35:20 -0600

    W01-Task completed.

--- w01-task.js
+++ w01-task.js
@@ -1,15 +1,15 @@
 // 🔍 Part 1 error
-const userName = "Moroni";
+let userName = "Moroni";
 console.log('Username: ${userName}');
 userName = "Moronihah";
 console.log('Username: ${userName}');
 
 // 🔍 Part 2 error
-const currentDateAndTime = DateTime;
+const currentDateAndTime = Date();
 console.log(`It is now ${currentDateAndTime}`);
 
-// 🔍 Part 3 error. The following statement calls a function named total that accepts any number of arguments and returns the sum. The returned value is stored in a variable named theTotal. 1-10 are the arguments.
-
+/* 🔍 Part 3 error. The following statement calls a function named total that accepts any number of arguments and 
+returns the sum. The returned value is stored in a variable named theTotal. 1-10 are the arguments. */
 let theTotal = total(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 console.log(`The total is ${theTotal}`);
 
