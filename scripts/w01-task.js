60f0a7bd5619179f566ae51dd0a99d85216268b7
Izzie Vazquez <izzievazquez@gmail.com>
2023-10-24 14:14:08 -0600

    Revised W01 Task.

--- scripts/w01-task.js
+++ scripts/w01-task.js
@@ -1,22 +1,22 @@
 // 🔍 Part 1 error
 let userName = "Moroni";
-console.log('Username: ${userName}');
+console.log(`Username: ${userName}`);
 userName = "Moronihah";
-console.log('Username: ${userName}');
+console.log(`Username: ${userName}`);
 
 // 🔍 Part 2 error
-const currentDateAndTime = Date();
-console.log(`It is now ${currentDateAndTime}`);
+const currentDateAndTime = new Date();
+console.log(`It is now ${currentDateAndTime}.`);
 
 /* 🔍 Part 3 error. The following statement calls a function named total that accepts any number of arguments and 
 returns the sum. The returned value is stored in a variable named theTotal. 1-10 are the arguments. */
 let theTotal = total(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-console.log(`The total is ${theTotal}`);
+console.log(`The total is ${theTotal}.`);
 
 // 'total' function declaration
 function total(...theNumbers) {
 	let sum = 0;
-	for (let aNumber in theNumbers) {
+	for (let aNumber of theNumbers) {
 		sum += aNumber * 1; // Why do we use * 1? It implicitly converts a string to a number.
 	}
 	return sum
